921ad444fc2b6422520ac2854005b3b573bef3d3
Coalis <coalis99@gmail.com>
2026-05-02 19:52:27 -0700

    phase1: Linux CMakeLists for portable core

---
+++ build-linux/CMakeFiles/xformer10.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/coalis/ClaudeCodeProjects/XFormer/xformer10/src/atari8.vm/atari800.c" "CMakeFiles/xformer10.dir/src/atari8.vm/atari800.c.o" "gcc" "CMakeFiles/xformer10.dir/src/atari8.vm/atari800.c.o.d"
+  "/home/coalis/ClaudeCodeProjects/XFormer/xformer10/src/atari8.vm/atarikey.c" "CMakeFiles/xformer10.dir/src/atari8.vm/atarikey.c.o" "gcc" "CMakeFiles/xformer10.dir/src/atari8.vm/atarikey.c.o.d"
+  "/home/coalis/ClaudeCodeProjects/XFormer/xformer10/src/atari8.vm/atariosb.c" "CMakeFiles/xformer10.dir/src/atari8.vm/atariosb.c.o" "gcc" "CMakeFiles/xformer10.dir/src/atari8.vm/atariosb.c.o.d"
+  "/home/coalis/ClaudeCodeProjects/XFormer/xformer10/src/atari8.vm/atarixl5.c" "CMakeFiles/xformer10.dir/src/atari8.vm/atarixl5.c.o" "gcc" "CMakeFiles/xformer10.dir/src/atari8.vm/atarixl5.c.o.d"
+  "/home/coalis/ClaudeCodeProjects/XFormer/xformer10/src/atari8.vm/atarixlb.c" "CMakeFiles/xformer10.dir/src/atari8.vm/atarixlb.c.o" "gcc" "CMakeFiles/xformer10.dir/src/atari8.vm/atarixlb.c.o.d"
+  "/home/coalis/ClaudeCodeProjects/XFormer/xformer10/src/atari8.vm/atarixlc.c" "CMakeFiles/xformer10.dir/src/atari8.vm/atarixlc.c.o" "gcc" "CMakeFiles/xformer10.dir/src/atari8.vm/atarixlc.c.o.d"
+  "/home/coalis/ClaudeCodeProjects/XFormer/xformer10/src/atari8.vm/atarixlo.c" "CMakeFiles/xformer10.dir/src/atari8.vm/atarixlo.c.o" "gcc" "CMakeFiles/xformer10.dir/src/atari8.vm/atarixlo.c.o.d"
+  "/home/coalis/ClaudeCodeProjects/XFormer/xformer10/src/atari8.vm/x6502.c" "CMakeFiles/xformer10.dir/src/atari8.vm/x6502.c.o" "gcc" "CMakeFiles/xformer10.dir/src/atari8.vm/x6502.c.o.d"
+  "/home/coalis/ClaudeCodeProjects/XFormer/xformer10/src/atari8.vm/xkey.c" "CMakeFiles/xformer10.dir/src/atari8.vm/xkey.c.o" "gcc" "CMakeFiles/xformer10.dir/src/atari8.vm/xkey.c.o.d"
+  "/home/coalis/ClaudeCodeProjects/XFormer/xformer10/src/atari8.vm/xlxeram.c" "CMakeFiles/xformer10.dir/src/atari8.vm/xlxeram.c.o" "gcc" "CMakeFiles/xformer10.dir/src/atari8.vm/xlxeram.c.o.d"
+  "/home/coalis/ClaudeCodeProjects/XFormer/xformer10/src/atari8.vm/xmon.c" "CMakeFiles/xformer10.dir/src/atari8.vm/xmon.c.o" "gcc" "CMakeFiles/xformer10.dir/src/atari8.vm/xmon.c.o.d"
+  "/home/coalis/ClaudeCodeProjects/XFormer/xformer10/src/atari8.vm/xsio.c" "CMakeFiles/xformer10.dir/src/atari8.vm/xsio.c.o" "gcc" "CMakeFiles/xformer10.dir/src/atari8.vm/xsio.c.o.d"
+  "/home/coalis/ClaudeCodeProjects/XFormer/xformer10/src/atari8.vm/xvideo.c" "CMakeFiles/xformer10.dir/src/atari8.vm/xvideo.c.o" "gcc" "CMakeFiles/xformer10.dir/src/atari8.vm/xvideo.c.o.d"
+  "/home/coalis/ClaudeCodeProjects/XFormer/xformer10/src/blocklib/blockapi.c" "CMakeFiles/xformer10.dir/src/blocklib/blockapi.c.o" "gcc" "CMakeFiles/xformer10.dir/src/blocklib/blockapi.c.o.d"
+  "/home/coalis/ClaudeCodeProjects/XFormer/xformer10/src/blocklib/mac_hfs.c" "CMakeFiles/xformer10.dir/src/blocklib/mac_hfs.c.o" "gcc" "CMakeFiles/xformer10.dir/src/blocklib/mac_hfs.c.o.d"
+  "/home/coalis/ClaudeCodeProjects/XFormer/xformer10/src/blocklib/sectorio.c" "CMakeFiles/xformer10.dir/src/blocklib/sectorio.c.o" "gcc" "CMakeFiles/xformer10.dir/src/blocklib/sectorio.c.o.d"
+  "/home/coalis/ClaudeCodeProjects/XFormer/xformer10/src/ddlib.c" "CMakeFiles/xformer10.dir/src/ddlib.c.o" "gcc" "CMakeFiles/xformer10.dir/src/ddlib.c.o.d"
+  "/home/coalis/ClaudeCodeProjects/XFormer/xformer10/src/gemul8r.c" "CMakeFiles/xformer10.dir/src/gemul8r.c.o" "gcc" "CMakeFiles/xformer10.dir/src/gemul8r.c.o.d"
+  "/home/coalis/ClaudeCodeProjects/XFormer/xformer10/src/main_linux.c" "CMakeFiles/xformer10.dir/src/main_linux.c.o" "gcc" "CMakeFiles/xformer10.dir/src/main_linux.c.o.d"
+  "/home/coalis/ClaudeCodeProjects/XFormer/xformer10/src/printer.c" "CMakeFiles/xformer10.dir/src/printer.c.o" "gcc" "CMakeFiles/xformer10.dir/src/printer.c.o.d"
+  "/home/coalis/ClaudeCodeProjects/XFormer/xformer10/src/props.c" "CMakeFiles/xformer10.dir/src/props.c.o" "gcc" "CMakeFiles/xformer10.dir/src/props.c.o.d"
+  "/home/coalis/ClaudeCodeProjects/XFormer/xformer10/src/romcard.c" "CMakeFiles/xformer10.dir/src/romcard.c.o" "gcc" "CMakeFiles/xformer10.dir/src/romcard.c.o.d"
+  "/home/coalis/ClaudeCodeProjects/XFormer/xformer10/src/serial.c" "CMakeFiles/xformer10.dir/src/serial.c.o" "gcc" "CMakeFiles/xformer10.dir/src/serial.c.o.d"
+  "/home/coalis/ClaudeCodeProjects/XFormer/xformer10/src/sound.c" "CMakeFiles/xformer10.dir/src/sound.c.o" "gcc" "CMakeFiles/xformer10.dir/src/sound.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
