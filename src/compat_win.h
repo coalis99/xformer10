f002e20e143689c006dbbd906607e5ffc10db7bb
Coalis <coalis99@gmail.com>
2026-05-02 20:00:51 -0700

    phase1: x6502.c compiles on Linux

--- src/compat_win.h
+++ src/compat_win.h
@@ -5,6 +5,7 @@
 #ifndef _WIN32
 
 #include <stdint.h>
+#include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
@@ -54,6 +55,34 @@ typedef double    DOUBLE;
 typedef uintptr_t DWORD_PTR;
 typedef uintptr_t ULONG_PTR;
 
+/* Message/pointer-sized types */
+typedef uintptr_t WPARAM;
+typedef intptr_t  LPARAM;
+typedef intptr_t  LRESULT;
+typedef intptr_t  LONG_PTR;
+typedef uintptr_t UINT_PTR;
+
+/* COM error type */
+typedef int32_t   HRESULT;
+#define S_OK         ((HRESULT)0)
+#define S_FALSE      ((HRESULT)1)
+#define E_FAIL       ((HRESULT)0x80004005)
+#define E_INVALIDARG ((HRESULT)0x80070057)
+#define E_NOTIMPL    ((HRESULT)0x80004001)
+#define SUCCEEDED(hr) ((HRESULT)(hr) >= 0)
+#define FAILED(hr)    ((HRESULT)(hr) < 0)
+
+/* Function pointer / string types */
+typedef void     *FARPROC;
+typedef const char *PCSTR;
+typedef const uint16_t *PCWSTR;
+typedef uint16_t *PWSTR;
+typedef BYTE     *PBYTE;
+typedef DWORD    *LPDWORD;
+
+/* MSVC 64-bit integer compat */
+typedef int64_t  __int64;
+
 typedef union _LARGE_INTEGER {
     struct { DWORD LowPart; LONG HighPart; };
     LONGLONG QuadPart;
@@ -69,6 +98,110 @@ typedef union _ULARGE_INTEGER {
 #endif
 #ifndef FALSE
 #define FALSE 0
+#endif
+
+/* GDI geometry types */
+typedef struct tagPOINT { LONG x; LONG y; } POINT, *PPOINT, *LPPOINT;
+typedef struct tagRECT  { LONG left; LONG top; LONG right; LONG bottom; } RECT, *PRECT, *LPRECT;
+typedef struct tagSIZE  { LONG cx; LONG cy; } SIZE, *PSIZE;
+
+/* GDI color/bitmap types */
+typedef struct tagRGBQUAD {
+    BYTE rgbBlue; BYTE rgbGreen; BYTE rgbRed; BYTE rgbReserved;
+} RGBQUAD;
+typedef struct tagBITMAPINFOHEADER {
+    DWORD biSize; LONG biWidth; LONG biHeight;
+    WORD  biPlanes; WORD biBitCount;
+    DWORD biCompression; DWORD biSizeImage;
+    LONG  biXPelsPerMeter; LONG biYPelsPerMeter;
+    DWORD biClrUsed; DWORD biClrImportant;
+} BITMAPINFOHEADER;
+typedef void *HPALETTE;
+
+/* DirectDraw stub (forward declaration for pointer use in gemtypes.h) */
+typedef struct IDirectDrawSurface IDirectDrawSurface;
+
+/* Multimedia result / audio types (stub structs for INST storage) */
+typedef UINT MMRESULT;
+#define MMSYSERR_NOERROR 0
+typedef struct tagWAVEOUTCAPS {
+    WORD  wMid; WORD wPid;
+    DWORD vDriverVersion;
+    char  szPname[32];
+    DWORD dwFormats;
+    WORD  wChannels; WORD wReserved1;
+    DWORD dwSupport;
+} WAVEOUTCAPS;
+#define WHDR_DONE     0x00000001u
+#define WHDR_PREPARED 0x00000002u
+typedef struct tagWAVEHDR {
+    char  *lpData;
+    DWORD  dwBufferLength;
+    DWORD  dwBytesRecorded;
+    DWORD_PTR dwUser;
+    DWORD  dwFlags;
+    DWORD  dwLoops;
+    struct tagWAVEHDR *lpNext;
+    DWORD_PTR reserved;
+} WAVEHDR;
+
+/* Joystick types (stub structs for INST storage) */
+#define JOY_RETURNALL 0x000000FFul
+typedef struct joyinfoex_tag {
+    DWORD dwSize; DWORD dwFlags;
+    DWORD dwXpos; DWORD dwYpos; DWORD dwZpos;
+    DWORD dwRpos; DWORD dwUpos; DWORD dwVpos;
+    DWORD dwButtons; DWORD dwButtonNumber;
+    DWORD dwPOV; DWORD dwReserved1; DWORD dwReserved2;
+} JOYINFOEX;
+typedef struct tagJOYCAPS {
+    WORD wMid; WORD wPid;
+    char szPname[32];
+    UINT wXmin; UINT wXmax; UINT wYmin; UINT wYmax;
+    UINT wZmin; UINT wZmax; UINT wNumButtons;
+    UINT wPeriodMin; UINT wPeriodMax;
+    UINT wRmin; UINT wRmax; UINT wUmin; UINT wUmax;
+    UINT wVmin; UINT wVmax;
+    UINT wCaps; UINT wMaxAxes; UINT wNumAxes; UINT wMaxButtons;
+    char szRegKey[32];
+    char szOEMVxD[260];
+} JOYCAPS;
+
+/* System info stub */
+typedef struct _SYSTEM_INFO {
+    DWORD     dwOemId;
+    DWORD     dwPageSize;
+    void     *lpMinimumApplicationAddress;
+    void     *lpMaximumApplicationAddress;
+    DWORD_PTR dwActiveProcessorMask;
+    DWORD     dwNumberOfProcessors;
+    DWORD     dwProcessorType;
+    DWORD     dwAllocationGranularity;
+    WORD      wProcessorLevel;
+    WORD      wProcessorRevision;
+} SYSTEM_INFO;
+
+/* Disk geometry stub (for blockdev.h GetDiskGeometry) */
+typedef struct _DISK_GEOMETRY {
+    LONGLONG Cylinders;
+    DWORD    MediaType;
+    DWORD    TracksPerCylinder;
+    DWORD    SectorsPerTrack;
+    DWORD    BytesPerSector;
+} DISK_GEOMETRY, *PDISK_GEOMETRY;
+
+/* MessageBox flags and stub (used in #ifndef NDEBUG _gem_assert in gemtypes.h) */
+#define MB_OK                  0x00000000u
+#define MB_ABORTRETRYIGNORE    0x00000002u
+#define MB_APPLMODAL           0x00000000u
+#define MB_ICONERROR           0x00000010u
+#define MB_ICONINFORMATION     0x00000040u
+static inline int MessageBox(void *hwnd, const char *text, const char *caption, unsigned flags)
+    { (void)hwnd; (void)flags; fprintf(stderr, "%s: %s\n", caption, text); return 3; } // PHASE3:
+static inline void *GetFocus(void) { return NULL; }
+#define wsprintf sprintf
+#ifndef __debugbreak
+#define __debugbreak() __builtin_trap()
 #endif
 
 /* Near-memory copy/set aliases */
