5211aca586f842898866c0260de1c9ad9251ded4
Coalis <coalis99@gmail.com>
2026-05-25 12:54:33 -0700

    phase13 step 13.4: multi-tile render in RenderBitmap_SDL
    Add tiling branch that loops cThreads, uploads each tile's pixel buffer,
    computes grid position, and draws white hover border. Single-VM path
    unchanged as else branch. Single shared argbBuf used for both paths.

--- src/ddlib_sdl.c
+++ src/ddlib_sdl.c
@@ -98,52 +98,84 @@ void UninitDrawing(BOOL fFinal)
 
 void RenderBitmap_SDL(void)
 {
+    if (gSDLTex == NULL || gSDLRen == NULL) return;
+
     static Uint32 argbBuf[X8 * Y8];
-    void *src = vvmhw.pbmTile[0].pvBits;
 
-    if (src == NULL || gSDLTex == NULL) return;
+    SDL_RenderClear(gSDLRen);
 
-    for (int i = 0; i < gTexW * gTexH; i++)
-    {
-        BYTE p = ((BYTE *)src)[i];
-        BYTE r = rgbRainbow[p * 3    ];
-        BYTE g = rgbRainbow[p * 3 + 1];
-        BYTE b = rgbRainbow[p * 3 + 2];
-        argbBuf[i] = (Uint32)0xFF000000
-            | (Uint32)((r << 2) | (r >> 5)) << 16
-            | (Uint32)((g << 2) | (g >> 5)) <<  8
-            | (Uint32)((b << 2) | (b >> 5));
-    }
+    if (v.fTiling && cThreads > 0) {
+        for (int t = 0; t < cThreads; t++) {
+            BYTE *src = (BYTE *)vvmhw.pbmTile[t].pvBits;
+            if (!src) continue;
 
-    SDL_UpdateTexture(gSDLTex, NULL, argbBuf, gTexW * 4);
+            for (int i = 0; i < gTexW * gTexH; i++) {
+                BYTE p = src[i];
+                BYTE r = rgbRainbow[p * 3    ];
+                BYTE g = rgbRainbow[p * 3 + 1];
+                BYTE b = rgbRainbow[p * 3 + 2];
+                argbBuf[i] = (Uint32)0xFF000000
+                    | (Uint32)((r << 2) | (r >> 5)) << 16
+                    | (Uint32)((g << 2) | (g >> 5)) <<  8
+                    | (Uint32)((b << 2) | (b >> 5));
+            }
+            SDL_UpdateTexture(gSDLTex, NULL, argbBuf, gTexW * 4);
 
-    SDL_Rect dest;
-    if (v.fZoomColor || v.fFullScreen) {
-        int winW, winH;
-        SDL_GetWindowSize(gSDLWin, &winW, &winH);
-        int availW = winW;
-        int availH = winH - MENU_H;
-        if (availH < 1) availH = 1;
-        if (v.fZoomColor) {
-            /* stretch: fill entire available area */
-            dest = (SDL_Rect){0, MENU_H, availW, availH};
-        } else {
-            /* fullscreen without stretch: letterbox proportionally */
-            int scaledW = availH * gTexW / gTexH;
-            if (scaledW <= availW)
-                dest = (SDL_Rect){(availW - scaledW) / 2, MENU_H, scaledW, availH};
-            else {
-                int scaledH = availW * gTexH / gTexW;
-                dest = (SDL_Rect){0, MENU_H + (availH - scaledH) / 2, availW, scaledH};
+            int slot = nFirstVisibleTile + t;
+            int col  = (sTilesPerRow > 0) ? slot % sTilesPerRow : 0;
+            int row  = (sTilesPerRow > 0) ? slot / sTilesPerRow : t;
+            SDL_Rect dest = {col * gTexW,
+                             MENU_H + v.sWheelOffset + row * gTexH,
+                             gTexW, gTexH};
+            SDL_RenderCopy(gSDLRen, gSDLTex, NULL, &dest);
+
+            if (sVM >= 0 && slot == sVM) {
+                SDL_SetRenderDrawColor(gSDLRen, 255, 255, 255, 255);
+                SDL_RenderDrawRect(gSDLRen, &dest);
+                SDL_SetRenderDrawColor(gSDLRen, 0, 0, 0, 255);
             }
         }
     } else {
-        /* normal windowed: fixed 3× */
-        dest = (SDL_Rect){0, MENU_H, gTexW * 3, gTexH * 3};
-    }
+        void *src = vvmhw.pbmTile[0].pvBits;
 
-    SDL_RenderClear(gSDLRen);
-    SDL_RenderCopy(gSDLRen, gSDLTex, NULL, &dest);
+        if (src == NULL) goto done;
+
+        for (int i = 0; i < gTexW * gTexH; i++) {
+            BYTE p = ((BYTE *)src)[i];
+            BYTE r = rgbRainbow[p * 3    ];
+            BYTE g = rgbRainbow[p * 3 + 1];
+            BYTE b = rgbRainbow[p * 3 + 2];
+            argbBuf[i] = (Uint32)0xFF000000
+                | (Uint32)((r << 2) | (r >> 5)) << 16
+                | (Uint32)((g << 2) | (g >> 5)) <<  8
+                | (Uint32)((b << 2) | (b >> 5));
+        }
+        SDL_UpdateTexture(gSDLTex, NULL, argbBuf, gTexW * 4);
+
+        SDL_Rect dest;
+        if (v.fZoomColor || v.fFullScreen) {
+            int winW, winH;
+            SDL_GetWindowSize(gSDLWin, &winW, &winH);
+            int availW = winW;
+            int availH = winH - MENU_H;
+            if (availH < 1) availH = 1;
+            if (v.fZoomColor) {
+                dest = (SDL_Rect){0, MENU_H, availW, availH};
+            } else {
+                int scaledW = availH * gTexW / gTexH;
+                if (scaledW <= availW)
+                    dest = (SDL_Rect){(availW - scaledW) / 2, MENU_H, scaledW, availH};
+                else {
+                    int scaledH = availW * gTexH / gTexW;
+                    dest = (SDL_Rect){0, MENU_H + (availH - scaledH) / 2, availW, scaledH};
+                }
+            }
+        } else {
+            dest = (SDL_Rect){0, MENU_H, gTexW * 3, gTexH * 3};
+        }
+        SDL_RenderCopy(gSDLRen, gSDLTex, NULL, &dest);
+    }
+done:
     MenuRender(gSDLRen);
     SDL_RenderPresent(gSDLRen);
 }
